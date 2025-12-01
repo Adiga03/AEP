0feaafef7735e129c7b393028549d58a544753d3
padiga <padiga@dollargeneral.com>
2025-12-01 17:44:55 +0530

    new lines added

--- src/main/java/login/login.java
+++ src/main/java/login/login.java
@@ -23,6 +23,8 @@ public class login {
                         By.xpath("//button[normalize-space() = 'Continue' or contains(., 'Continue') or @id='continue' or @name='continue']")));
         continueBtn.click();
         System.out.println("Clicked Continue button");
+        System.out.println("Login process initiated.");
+        System.out.println("Please complete any further steps manually if required.");
 
     }
 }
